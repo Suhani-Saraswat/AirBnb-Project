53f82372b53b9fe0e160e1d6745d4a9924c7e4e0
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-07 21:44:17 +0530

    Basic data.js and listing.js file

---
+++ models/listing.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ListingSchema = new Schema({
+    title: String,
+    description: String,
+    image: String,
+    
+})
