7a63025abce691d4e1d247b94bc40306b1610cdf
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-08 14:46:36 +0530

    listing.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride = require("method-override");
+// const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -19,20 +19,20 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 // app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride("_method"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
 
-// //Index Route
-// app.get("/listings", async (req, res) => {
-//   const allListings = await Listing.find({});
-//   res.render("listings/index.ejs", { allListings });
-// });
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
 // //New Route
 // app.get("/listings/new", (req, res) => {
@@ -75,19 +75,19 @@ async function main() {
 //   res.redirect("/listings");
 // });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("successful testing");
-});
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
