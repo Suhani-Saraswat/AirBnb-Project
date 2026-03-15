dd9029880ac32722a7ffa93fef6b84daf8301a4b
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-15 21:53:24 +0530

    updated listing.js

--- models/listing.js
+++ models/listing.js
@@ -8,8 +8,6 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String,
-    url: {
         type: String,
         default:
         "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
@@ -17,7 +15,6 @@ const listingSchema = new Schema({
         v === ""
             ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
             : v,
-    }
   },
   price: Number,
   location: String,
