3c359cd1502abe2b5801cad0612b33c97f9f3ee4
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-17 22:15:38 +0530

    success flash for all

--- routes/listing.js
+++ routes/listing.js
@@ -63,6 +63,7 @@ router.put("/:id",
   wrapAsync (async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   })
 );
@@ -73,6 +74,7 @@ router.delete("./:id",
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   })
 );
--- routes/review.js
+++ routes/review.js
@@ -28,6 +28,8 @@ router.post("/",
     await newReview.save();
     await listing.save();
 
+    req.flash("success", "New Review Created!");
+
     res.redirect(`/listings/${listing._id}`);
   })
 );
@@ -39,6 +41,8 @@ router.delete("/:reviewId",
 
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
+
+    req.flash("success", "Review Deleted!");
 
     res.redirect(`/listings/${id}`);
   })
