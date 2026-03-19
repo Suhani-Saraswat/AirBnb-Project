d3d87440b2aa7d5004971ce58aac180002ab8291
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-19 21:51:52 +0530

    listing owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69bac71b6b37d30e2c6de318",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
