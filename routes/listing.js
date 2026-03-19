25fe78008c97b8dd90424cb013e6f5f636ddc9af
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-19 22:35:35 +0530

    listing authorization

--- routes/listing.js
+++ routes/listing.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn } = require("../middleware.js");
-
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 //Index Route
 router.get("/", 
@@ -59,7 +47,8 @@ router.post("/",
 
 //Edit Route
 router.get("/:id/edit",
-  isLoggedIn,  
+  isLoggedIn, 
+  isOwner, 
   wrapAsync (async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -74,6 +63,7 @@ router.get("/:id/edit",
 //Update Route
 router.put("/:id", 
   isLoggedIn,
+  isOwner, 
   validateListing,
   wrapAsync (async (req, res) => {
     let { id } = req.params;
@@ -86,6 +76,7 @@ router.put("/:id",
 //Delete Route
 router.delete("/:id", 
   isLoggedIn,
+  isOwner, 
   wrapAsync (async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
